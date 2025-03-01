182531319dd8d439d53e5e25bf6668e9bcc82b2d
Ahmet Şahin <84666508+ahmetdsahin@users.noreply.github.com>
2025-03-02 00:38:48 +0300

    update

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { assets } from "../../assets/assets.js";
+import "./Sidebar.css";
+
+const Sidebar = () => {
+
+    const [extended,setExtended] = useState(false);
+
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <img onClick={()=> setExtended(prev=>!prev)} className="menu" src={assets.menu_icon} alt="" />
+        <div className="new-chat">
+          <img src={assets.plus_icon} alt="" />
+         {extended?<p>Yeni sohbet</p>:null} 
+        </div>
+        {extended
+        ?<div className="recent">
+        <p className="recent-title">En son</p>
+        <div className="recent-entry">
+          <img src={assets.message_icon} alt="" />
+          <p>Türkiye'nin başkenti...</p>
+        </div>
+      </div>
+      :null
+        }
+        
+      </div>
+      <div className="bottom">
+        <div className="bottom-item recent-entry">
+          <img src={assets.question_icon} alt="" />
+         {extended?<p>Yardım</p>:null} 
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.history_icon} alt="" />
+          {extended?<p>Etkinlik</p>:null}
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.setting_icon} alt="" />
+         {extended?<p>Ayarlar</p> :null} 
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
